81b7bac277e4b24baf75c0661d9ec030d405ea74
ALUNO <ALUNO@LABHPC24>
2024-02-27 17:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ exerc3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+            Console.WriteLine("Digite apenas a primeira letra do seu sexo: ");
+            sexo = char.Parse(Console.ReadLine());
+
+            if (sexo == 'M' || sexo == 'F')
+            { 
+                Console.WriteLine("sua resposta foi válida"); 
+            } 
+            Console.ReadKey();
+
+        }
+    }
+}
